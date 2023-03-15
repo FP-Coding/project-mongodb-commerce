d86598cf0ea02cb6ef8df8eb68cf55dbe2dbcd50
Felipe Pinto <felipe14062000@gmail.com>
2023-03-15 15:29:02 -0300

    lint: fix errors

--- challenges/desafio19.js
+++ challenges/desafio19.js
@@ -1,3 +1,6 @@
-db.produtos.updateMany({ ingredientes: { $all: ["cebola"] } }, { $pull: { ingredientes: { $eq: "cebola" } } });
+db.produtos.updateMany(
+  { ingredientes: { $all: ["cebola"] } },
+  { $pull: { ingredientes: { $eq: "cebola" } } },
+);
 
 db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });
